c5d5e2f9f8929e01e39959d898cc400dd2b03c1d
Dante-Argenis-2 <argrnis06pollo@gmail.com>
2025-06-26 13:20:07 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Bottling Tacticians</h1>
+      <p>This is a multiplayer React game with 50 tiles and special tile effects.</p>
+      <p>Coming soon: Full board, avatars, turn logic, and more.</p>
+    </div>
+  );
+}
+
+export default App;
